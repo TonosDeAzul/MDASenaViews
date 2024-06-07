033698ee6ff2577dc4cc8bb718fcdb80458980a5
TonosDeAzul <dg.wrq109@gmail.com>
2024-06-07 14:39:52 -0500

    inicio del desarrollo de las vistas

---
+++ scripts/tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        mdaGreen: "#39A900",
+        mdaBlack: "#1D1D1D",
+        mdaWhite: "#F1F1F1",
+        mdaRed: "#F80000",
+      },
+    },
+  },
+};
