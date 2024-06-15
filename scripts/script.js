c241b67e79ff4107d09b76ad01b1206f84a4c2db
TonosDeAzul <dg.wrq109@gmail.com>
2024-06-14 22:19:13 -0500

    se agregó el modal de notificaciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,28 @@
+// MODAL PARA AGREGAR MONITOR
 const modal = document.getElementById("modal-1");
+if(modal !== null){
+  const showModal = document.getElementById("showModal");
+  showModal.onclick = function () {
+    modal.classList.remove("hidden");
+  };
+  const closeModal = document.getElementById("closeModal");
+  closeModal.onclick = function () {
+    modal.classList.add("hidden");
+  };
+}
 
-const showModal = document.getElementById("showModal");
-showModal.onclick = function () {
-  modal.classList.remove("hidden");
-};
-
-const closeModal = document.getElementById("closeModal");
-closeModal.onclick = function () {
-  modal.classList.add("hidden");
-};
+// MODAL PARA NOTIFICACIONES
+const modal_2 = document.getElementById("modal-2");
+const modal_2__back = document.getElementById("modal-2__background");
+if(modal_2 !== null){
+  const showModal_2 = document.getElementById("showModal-2");
+  showModal_2.onclick = function () {
+    modal_2.classList.remove("hidden");
+    modal_2__back.classList.remove("hidden");
+  };
+  const closeModal_2 = document.getElementById("closeModal-2");
+  closeModal_2.onclick = function () {
+    modal_2.classList.add("hidden");
+    modal_2__back.classList.add("hidden");
+  };
+}
