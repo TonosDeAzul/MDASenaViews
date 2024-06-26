36d8f3fdba899a70a7420247cf73796dd49b86c2
TonosDeAzul <dg.wrq109@gmail.com>
2024-06-26 17:19:11 -0500

    se agregaron los fondos

--- scripts/tailwind.js
+++ scripts/tailwind.js
@@ -8,6 +8,13 @@ tailwind.config = {
         mdaRed: "#F80000",
         mdaGreen_400: "#39A90040",
         mdaBlack_400: "#1D1D1D40",
+        mdaBlack_600: "#1D1D1D60",
+      },
+      backgroundImage: {
+        background1: "url('../images/background1.jpg')",
+        background2: "url('../images/background2.jpg')",
+        background3: "url('../images/background3.jpg')",
+        background4: "url('../images/background4.jpg')",
       },
     },
   },
